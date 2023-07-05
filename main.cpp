e5502925f2678cc3e26ee6995b434ff0b5d887c3
yyyy6365 <yyyy6365@naver.com>
2023-07-05 01:28:53 -0400

    edit main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,34 @@
-#include<stdint.h>
+#include<stdio.h>
 
+int main(){
+
+	FILE* fp_t;
+	FILE* fp_fh
+	int a=0;
+
+	fp_t = fopen("t.bin","rb");
+	fp_fh = fopen("fh.bin","rb");
+
+	if(fp_t == NULL)
+	{
+		printf("NO FILE.");
+		return 0;
+	}
+
+	if(fp_fh == NULL)
+	{
+		printf("NO FILE.");
+	}
+
+	char str[10];
+	
+	uint32_t r_a = fread(&a, sizeof(int),1,fp_t );
+	uint32_t r_b = fread(&b, sizeof(int),1,fp_fh);
+
+	      printf("%d %d",r_a,r_b);
+
+	fclose(fp_t);
+	fclose(fp_fh);
+
+}
 
